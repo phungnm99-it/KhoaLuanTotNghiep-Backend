675950ccf45b51903cf26d3890b6bd3f4327df6a
Nguyen Minh Phung <phungnm99@gmail.com>
2022-01-08 21:35:00 +0700

    ỉmprove code

--- WebAPI/Utils/JwtUtils.cs
+++ WebAPI/Utils/JwtUtils.cs
@@ -36,7 +36,7 @@ namespace WebAPI.Utils
                     new Claim(ClaimTypes.Name, user.Id.ToString()),
                     new Claim(ClaimTypes.Role, user.RoleName)
                 }),
-                Expires = DateTime.UtcNow.AddHours(6),
+                Expires = DateTime.UtcNow.AddHours(12),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
